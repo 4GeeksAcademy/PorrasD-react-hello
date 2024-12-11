2c3ff72a0f944e84b601dd3a9aae3f966b4b51d9
PorrasD <122325175+PorrasD@users.noreply.github.com>
2024-12-11 04:37:54 +0000

    version2

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -7,21 +7,21 @@ const Card = () => {
         <div className=" col card">
           <img src="..." className="card-img-top"  />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title d-flex justify-content-center">Card title</h5>
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
           </div>
-          <div className="card-footer">
-            <button className="btn btn-primary btn-lg" type="button">
+          <div className="card-footer d-flex justify-content-center">
+            <button className="btn btn-primary btn-lg " type="button">
               Find out more!
             </button>            </div>
         </div>
         <div className=" col card">
           <img src="..." className="card-img-top"  />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title d-flex justify-content-center">Card title</h5>
             <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
           </div>
-          <div className="card-footer">
+          <div className="card-footer d-flex justify-content-center">
           <button className="btn btn-primary btn-lg" type="button">
               Find out more!
             </button>            </div>
@@ -29,10 +29,10 @@ const Card = () => {
         <div className=" col card">
           <img src="..." className="card-img-top"  />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title d-flex justify-content-center">Card title</h5>
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
           </div>
-          <div className="card-footer">
+          <div className="card-footer d-flex justify-content-center">
           <button className="btn btn-primary btn-lg" type="button">
               Find out more!
             </button>            </div>
@@ -40,10 +40,10 @@ const Card = () => {
         <div className=" col card">
           <img src="..." className="card-img-top"  />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title d-flex justify-content-center">Card title</h5>
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
           </div>
-          <div className="card-footer">
+          <div className="card-footer d-flex justify-content-center">
           <button className="btn btn-primary btn-lg" type="button">
               Find out more!
             </button>            
--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="bg-dark text-center py-4">
+        <div className="bg-dark text-center py-4 ">
             <div className="container text-white">
                 <p> <small>Copyright ©; Your Website 2024</small></p>
             </div>
